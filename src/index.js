88ed300e70fddd0be078bea1c61b2a827f7a0adb
Po <po.grammer.lin@gmail.com>
2017-11-14 15:51:33 -0600

    Completed Bonus Pending, Modal, & Genre Functionalities

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@
  */
 import sayHello from './hello';
 import getMovies from './api.js';
-import $ from 'jquery';
 sayHello('World');
 
 /**
@@ -14,6 +13,39 @@ sayHello('World');
 let stars = 0;
 let editing = false;
 let currId;
+let currGenre = [];
+
+const genres = ["Drama", "Horror", "Action", "Western", "Thriller", "Adventure", "Documentary", "Animation", "Romantic Comedy", "Fiction", "War Film", "Film Noir", "Science Fiction", "Romance", "Musical", "Family", "Fantasy", "Disaster Film", "Biographical", "Melodrama", "Slasher", "Spy", "Art", "Mystery", "B Movie", "Gangster", "Black Comedy", "Chick Flick", "Crime", "Comedy Horror", "Mockumentary", "Crime Fiction", "Cult", "Heist", "Neo-Noir", "Anime", "Indie", "Blaxpoitation", "Parody", "Peplum", "Satire"];
+
+const uncheckBoxes = () => {
+    $('.genre-checkbox').each((index, element) => {
+        element.checked = false;
+    });
+    currGenre = [];
+};
+
+const getIdFromStr = (str) => {
+    let holder = "";
+    let retval = "";
+    for(let i = 0; i < str.length; i++) {
+        if(holder === 'btn-edit-' || holder === 'btn-delete-') {
+            retval += str[i];
+        } else {
+            holder += str[i];
+        }
+    }
+    return retval;
+};
+
+const populateGenreCheckboxes = () => {
+    let inputStr = "";
+    genres.forEach((genre) => {
+        inputStr += '<label class="col-sm-3">';
+        inputStr += `<input id="genre-${genre.split(' ').join('-')}" class="genre-checkbox" type="checkbox" value="${genre}">${genre}`;
+        inputStr += '</label>';
+    });
+    $('.checkbox').html(inputStr);
+};
 
 const fetchMovies = () => {
     $.ajax('/api/movies')
@@ -23,6 +55,7 @@ const fetchMovies = () => {
             $('#movies-insert').removeClass('text-center').addClass('text-left');
             datas.forEach(data => {
                 let moviesStr = "";
+                moviesStr += `<div class="movie-badges col-md-6">`;
                 moviesStr += `<div class="panel panel-primary">`;
                 moviesStr += `<div class="panel-heading">`;
                 moviesStr += `<h3 id="movie-title-${data.id}" class="panel-title">${data.title}</h3>`;
@@ -31,7 +64,13 @@ const fetchMovies = () => {
                 for(var i = 0; i < parseInt(data.rating); i++) {
                     moviesStr += `<span class="glyphicon glyphicon-star" aria-hidden="true"></span>`;
                 }
-                moviesStr += `<div class="text-right"><div class="btn-group"><button id="btn-edit-${data.id}" class="btn-edit btn btn-sm btn-default">Edit</button><button id="btn-delete-${data.id}" class="btn-delete btn btn-sm btn-danger">Delete</button></div></div>`
+                moviesStr += `<div id="movie-genres-${data.id}" class="movie-badge-genre">`;
+                data.genre.forEach(elem => {
+                    moviesStr += `<span class="label label-info">${genres[elem]}</span>`;
+                });
+                moviesStr += `</div>`;
+                moviesStr += `<div class="text-right"><div class="btn-group" id="btn-group-${data.id}"><button id="btn-edit-${data.id}" class="btn-edit btn btn-sm btn-default">Edit</button><button id="btn-delete-${data.id}" class="btn-delete btn btn-sm btn-danger">Delete</button></div></div>`
+                moviesStr += `</div>`;
                 moviesStr += `</div>`;
                 moviesStr += `</div>`;
                 $('#movies-insert').append(moviesStr);
@@ -40,7 +79,10 @@ const fetchMovies = () => {
             $('.btn-delete').click((e) => {
                 console.log('deleting');
                 let clickedId = $(e.target).attr('id');
-                clickedId = parseInt(clickedId[clickedId.length-1]);
+                clickedId = parseInt(getIdFromStr(clickedId));
+
+                $(`#btn-group-${clickedId}`).html(`<button class="btn btn-sm btn-default" disabled>Pending</button>`);
+
                 const url = `/api/movies/${clickedId}`;
                 const options = {
                     method: 'DELETE'
@@ -60,9 +102,13 @@ const fetchMovies = () => {
             $('.btn-edit').click((e) => {
                 console.log('editing');
                 let clickedId = $(e.target).attr('id');
-                clickedId = parseInt(clickedId[clickedId.length-1]);
+                clickedId = parseInt(getIdFromStr(clickedId));
                 $('#movie-title-input').val($(`#movie-title-${clickedId}`).html());
-                let nth = $(`#movie-ratings-${clickedId} span`).length;
+                $(`#movie-genres-${clickedId}>span`).each((index, element) => {
+                    currGenre.push(genres.indexOf($(element).html()));
+                    $(`#genre-${$(element).html().split(' ').join('-')}`)[0].checked = true;
+                });
+                let nth = $(`#movie-ratings-${clickedId}>span`).length;
 
                 $(`#rating-area>span:nth-child(${nth})`).css('color', 'yellow').removeClass('glyphicon-star-empty').addClass('glyphicon-star');
                 $(`#rating-area>span:nth-child(${nth})`).prevAll().css('color', 'yellow').removeClass('glyphicon-star-empty').addClass('glyphicon-star');
@@ -73,6 +119,8 @@ const fetchMovies = () => {
                 currId = clickedId;
                 $('#movies-form-submit-btn').html('Edit');
                 $('#movies-form-submit-btn').removeClass('btn-primary').addClass('btn-success');
+
+                $('#movieFormModal').modal("show");
             });
         })
         .fail(error => {
@@ -94,12 +142,32 @@ window.onload = () => {
     $('#movies-insert').html('Loading...');
     //console.log('loading...');
     fetchMovies();
+    populateGenreCheckboxes();
+
+    $('.genre-checkbox').click((e) => {
+        if(!$(e.target)[0].checked) {
+            currGenre.splice(currGenre.indexOf(genres.indexOf(e.target.value)), 1);
+        } else {
+            currGenre.push(genres.indexOf(e.target.value));
+        }
+    });
 };
+
+$('#movies-form-cancel-btn').click(() => {
+    $('#movies-form-submit-btn').html('Submit');
+    $('#movies-form-submit-btn').removeClass('btn-success').addClass('btn-primary');
+    stars = 0;
+    $('#movieFormModal').modal("hide");
+    $('#movie-title-input').val('');
+    $('.star').css('color', 'black').removeClass('glyphicon-star').addClass('glyphicon-star-empty');
+    editing = false;
+    uncheckBoxes();
+});
 
 $('#movies-form-submit-btn').click(() => {
     if($('#movie-title-input').val() !== '') {
         if(!editing) {
-            const movies = {title: $('#movie-title-input').val(), rating: stars};
+            const movies = {title: $('#movie-title-input').val(), rating: stars, genre: currGenre};
             console.log(movies);
             const url = '/api/movies';
             const options = {
@@ -119,7 +187,9 @@ $('#movies-form-submit-btn').click(() => {
                     console.log(error);
                 });
         } else {
-            const movies = {title: $('#movie-title-input').val(), rating: stars};
+            $(`#btn-group-${currId}`).html(`<button class="btn btn-sm btn-default" disabled>Pending</button>`);
+
+            const movies = {title: $('#movie-title-input').val(), rating: stars, genre: currGenre};
             console.log(movies);
             const url = `/api/movies/${currId}`;
             const options = {
@@ -147,6 +217,9 @@ $('#movies-form-submit-btn').click(() => {
     $('#movie-title-input').val('');
     $('.star').css('color', 'black').removeClass('glyphicon-star').addClass('glyphicon-star-empty');
     stars = 0;
+
+    $('#movieFormModal').modal("hide");
+    uncheckBoxes();
 });
 
 $('.star').click((e) => {
